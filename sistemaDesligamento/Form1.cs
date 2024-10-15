a49a5eea1f0dbb8b7b95590e9f90b9c72831be6c
nath-boo <nathalia.gmansano@senacsp.edu.br>
2024-10-15 20:15:03 -0300

    adicionei a biblioteca

--- sistemaDesligamento/Form1.cs
+++ sistemaDesligamento/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;// Biblioteca para Diagnóstico do sistema
 
 namespace sistemaDesligamento
 {
